004cb44aad2417f420e539d27b233f38d9e0951e
gravdanger510e <gravdanger510@gmail.com>
2016-03-31 15:07:27 -0400

    converted repo to wp theme

---
+++ front-page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The front page of the site. Should show up automatically
+ *
+ *
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
